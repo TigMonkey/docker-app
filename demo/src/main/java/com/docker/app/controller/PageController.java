1f8a08da1131abd7b274386d88bcde752c7e63a9
yhnkw <yhnkw@DESKTOP-RQHJL7H>
2019-04-20 20:47:49 +0900

    docker boot

---
+++ demo/src/main/java/com/docker/app/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.docker.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+	@GetMapping("/")
+	public String main() {
+		final String PAGE_NAME = "docker";
+		return PAGE_NAME;
+	}
+	
+}
